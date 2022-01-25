a79a8cb72c38462ea6f4555675cd1dd9f955d7f9
Jérémie <73988354+SK8gh@users.noreply.github.com>
2022-01-25 23:33:00 +0100

    Optimization to get the relative positions of the subway stations, Building the class to plot maps

--- configuration.py
+++ configuration.py
@@ -1,3 +1,9 @@
 path_coordinates = '/Users/miller/Desktop/MAP2/data/dict_coord.npy'
 
 path_distances = '/Users/miller/Desktop/MAP2/data/distances.npy'
+
+places_sub_list = places = ['Châtelet', 'Nation', 'Château de Vincennes', 'CDG', 'Opéra', 'Saint-Lazare', 'Vavin',
+         "Gare d'Austerlitz", 'Dupleix', 'Invalides', "Place d'Italie", 'Belleville', 'Ourcq',
+         'Trocadéro', 'Montparnasse', 'Montrouge', 'Gare du Nord', 'Anatole France', 'Gambetta', 'Cité', 'Pasteur',
+         'République', 'Glacière', 'Kléber', 'Place de Clichy', 'Porte de la Chapelle']
+
--- get_distances.py
+++ get_distances.py
@@ -21,17 +21,20 @@ from heap import Heap
 import warnings
 warnings.filterwarnings("ignore")
 
-dict_coordinates = np.load(conf.path_coordinates, allow_pickle=True).reshape(1)[0]
-places_names = list(dict_coordinates.keys())
+dict_coordinates_load = np.load(conf.path_coordinates, allow_pickle=True).reshape(1)[0]
+places_names = conf.places_sub_list
+
+# Reducing the size of the coordinates dictionary
+dict_coordinates = {place : dict_coordinates_load[place] for place in places_names}
 
 
 class GetDistances:
     def __init__(self, coordinates, places):
-        self.heap_elt_tries = {}
         self.coordinates = coordinates
         self.places = places
 
         self.distances = np.zeros([len(places), len(places)])
+        self.count = 0
 
     def add_attr_heap(self):
         heap = Heap()
@@ -39,8 +42,6 @@ class GetDistances:
             for j, place_j in enumerate(self.places):
                 if i > j:
                     heap.heappush((i, j))
-                    self.heap_elt_tries[(i, j)] = 0
-
         self.heap = heap
 
     def thread_target(self):
@@ -49,14 +50,8 @@ class GetDistances:
 
         while self.heap:
             index1, index2 = self.heap.heappop()
+            self.count += 1
             try:
-                self.heap_elt_tries[(index1, index2)] += 1
-
-                logger = logging.getLogger('WazeRouteCalculator.WazeRouteCalculator')
-                logger.setLevel(logging.DEBUG)
-                handler = logging.StreamHandler()
-                logger.addHandler(handler)
-
                 from_address = self.coordinates[self.places[index1]]
                 to_address = self.coordinates[self.places[index2]]
                 region = 'EU'
@@ -64,18 +59,18 @@ class GetDistances:
 
                 self.distances[index1, index2] = route.calc_route_info()[0]
 
-            except Exception as e:
-                print(f"An exception was raised for places : {self.places[index1], self.places[index2]}")
-                if self.heap_elt_tries[(index1, index2)] < 3:
-                    self.heap.heappush((index1, index2))
-        time.sleep(0.1)
+                print(f"Distance from {self.places[index1]} to {self.places[index2]} = {self.distances[index1, index2]}")
+
+            except Exception:
+                pass
+
+            finally:
+                time.sleep(1)
 
     def run_threads(self, nb_threads):
         threads = []
 
         self.add_attr_heap()
-
-        print(type(self.heap))
 
         for _ in range(nb_threads):
             t = threading.Thread(target=GetDistances.thread_target, args=(self,), daemon=True)
@@ -88,15 +83,49 @@ class GetDistances:
             t.join()
 
 
-def go():
-    number_threads = 25
-    GetDistancesInstance = GetDistances(dict_coordinates, places_names)
-    GetDistancesInstance.run_threads(number_threads)
-    np.save('/Users/miller/Desktop/MAP2/data/distances.npy', GetDistancesInstance.distances)
+def get_positions():
+    times = np.load('/Users/miller/Desktop/MAP2/data/distances.npy', allow_pickle=True)
+    len_times = len(times)
 
-go()
+    def get_error(args):
+        error = 0
+        x, y = args[:len_times], args[len_times:]
 
-dist = np.load('/Users/miller/Desktop/MAP2/data/distances.npy', allow_pickle=True)
+        for i_1, x_val in enumerate(x):
+            for i_2, y_val in enumerate(y):
+                if times[i_1, i_2] != 0:
+                    error += abs((x[i_1] - x[i_2]) ** 2 + (y[i_1] - y[i_2]) ** 2 - times[i_1, i_2] ** 2)
+        return error
 
-for row in dist:
-    print(row, "\n")
+    args0 = np.zeros([2 * len_times])
+
+    min_result = minimize(get_error, args0, method="Powell", tol=1e-6)
+    return min_result
+
+
+class PlotMap:
+    @staticmethod
+    def plot():
+        pts_values = get_positions().x
+        len_values = int(len(pts_values) / 2)
+
+        x, y = pts_values[:len_values], pts_values[len_values:]
+
+        fig, ax = plt.subplots()
+
+        for i in range(len_values):
+            ax.scatter(-x[i], -y[i], color='purple')
+
+        for i in range(len_values):
+            ax.annotate(places_names[i], (-x[i], -y[i]))
+
+        plt.legend()
+        plt.show()
+
+
+if __name__ == '__main__':
+
+    PlotMap.plot()
+
+
+
